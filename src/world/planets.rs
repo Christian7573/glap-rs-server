58683e7e859f00088166b364d60d9a88ac885085
Christian7573 <cag4561@gmail.com>
2020-08-25 21:12:11 -0500

    Added a moon maybe; Updated earth's mass

--- src/world/planets.rs
+++ src/world/planets.rs
@@ -5,16 +5,18 @@ use ncollide2d::shape::{Ball, ShapeHandle};
 use nalgebra::Vector2;
 
 pub struct Planets {
-    pub earth: CelestialObject
+    pub earth: CelestialObject,
+    pub moon: CelestialObject,
 }
 impl Planets {
     pub fn new(colliders: &mut super::MyColliderSet, bodies: &mut super::World) -> Planets {
+        const EARTH_MASS: f32 = 700.0;
         let earth = {
             let body = RigidBodyDesc::new()
                 .translation(Vector2::new(0.0,0.0))
                 .gravity_enabled(false)
                 .status(BodyStatus::Static)
-                .mass(500.0)
+                .mass(EARTH_MASS)
                 .build();
             let body_handle = bodies.add_celestial_object(body);
             const RADIUS: f32 = 25.0;
@@ -35,13 +37,39 @@ impl Planets {
             }
         };
 
+        let moon = {
+            let body = RigidBodyDesc::new()
+                .translation(Vector2::new(42.4530083832,69.2770133538))
+                .gravity_enabled(false)
+                .status(BodyStatus::Static)
+                .mass(EARTH_MASS / 81.0)
+                .build();
+            let body_handle = bodies.add_celestial_object(body);
+            const RADIUS: f32 = 25.0 / 4.0;
+            let shape = ShapeHandle::new(Ball::new(RADIUS));
+            let collider = ColliderDesc::new(shape)
+                //.margin()
+                .build(BodyPartHandle(body_handle, 0));
+            colliders.insert(collider);
+
+            let id = if let MyHandle::CelestialObject(id) = body_handle { id } else { panic!() };
+            
+            CelestialObject {
+                name: String::from("earth"),
+                display_name: String::from("Moon"),
+                radius: RADIUS,
+                body: body_handle,
+                id
+            }
+        };
+
         Planets {
-            earth
+            earth, moon
         }
     }
 
-    pub fn celestial_objects<'a>(&'a self) -> [&'a CelestialObject; 1] {
-        [&self.earth]
+    pub fn celestial_objects<'a>(&'a self) -> [&'a CelestialObject; 2] {
+        [&self.earth, &self.moon]
     }
 }
 
