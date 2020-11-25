eb5da737a36eef303b8faf941279b60d93bf2cab
Christian7573 <cag4561@gmail.com>
2020-11-25 15:13:36 -0600

    Finished TcpStreamWrapper, replaced PotentialWebsocket with an async function

--- src/session/websocket.rs
+++ src/session/websocket.rs
@@ -1,6 +1,6 @@
 use async_std::net::TcpStream;
 use std::collections::VecDeque;
-use futures::{Future, AsyncRead, AsyncWrite, Stream, Sink};
+use futures::{Future, AsyncRead, AsyncWrite, Stream, Sink, StreamExt};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::ops::{Deref, DerefMut};
@@ -29,6 +29,9 @@ impl From<TcpStream> for TcpStreamWrapper {
 impl Stream for TcpStreamWrapper {
     type Item = u8;
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        //Writing
+        self.async_write(cx);
+
         //Reading
         //Data is waiting to surface to the user
         if let Some((input_index, input_end)) = self.input_slice {
@@ -59,11 +62,11 @@ impl Stream for TcpStreamWrapper {
 }
 
 impl TcpStreamWrapper {
-    fn queue_output(&mut self, dat: Arc<Vec<u8>>) {
+    pub fn queue_send(&mut self, dat: Arc<Vec<u8>>) {
         self.output.push_back(dat);
     }
     
-    fn poll_write(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(),()>> {
+    fn async_write(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(),()>> {
         //Writing
         if !self.output.is_empty() {
             //Position where the past write finished
@@ -92,238 +95,115 @@ impl TcpStreamWrapper {
             Poll::Ready(Ok(()))
         }
     }
+
+    pub fn flush(&mut self) -> TcpStreamWrapperFlushFuture {
+        TcpStreamWrapperFlushFuture ( self )
+    }
+
+    pub fn unwarp(self) -> TcpStream { self.socket }
 }
 
-struct TcpStreamWrapperFlushFuture<'a> ( &'a mut TcpStreamWrapper );
+pub struct TcpStreamWrapperFlushFuture<'a> ( &'a mut TcpStreamWrapper );
 impl<'a> Future for TcpStreamWrapperFlushFuture<'a> {
     type Output = ();
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<()> {
-        match Pin::new(&mut self.0).poll_write(cx) {
+        match Pin::new(self.0).async_write(cx) {
             Poll::Ready(Ok(())) => {
                 if !self.0.output.is_empty() { Poll::Ready(()) }
                 else { Poll::Pending }
             },
             Poll::Ready(Err(())) => Poll::Ready(()),
             Poll::Pending => Poll::Pending
-    }
-}
-
-
-pub struct PotentialWebsocket {
-    socket: Option<TcpStream>,
-    state: PotentialWebsocketState,
-    encryption_thing: Option<[u8; 16]>,
-    wants_upgrade: bool,
-    wants_websocket: bool,
-    wants_websocket_13: bool,
-}
-
-enum PotentialWebsocketState {
-    ReadingHttpMethod { chars_read: u8 },
-    ReadingHttpUri,
-    ReadingHttpVersion,
-
-    ReadingHttpHeaderName { name: String },
-    SkippingHttpHeader,
-    ReadingHttpUpgradeHeader { value: String },
-    ReadingHttpConnectionHeader { value: String },
-    ReadingWsEncryptionHeader { value: String },
-    ReadingWsVersion { value: String },
-
-    //FlushingData { data: Vec<u8>, next: Box<PotentialWebsocketState> },
-    FlushingDataThenAccept { data: Vec<u8> },
-    Temp
-}
-
-impl PotentialWebsocket {
-    pub fn new(stream: TcpStream) -> Self {
-        PotentialWebsocket {
-            socket: Some(stream),
-            state: PotentialWebsocketState::ReadingHttpMethod { chars_read: 0 },
-            encryption_thing: None,
-            wants_upgrade: false,
-            wants_websocket: false,
-            wants_websocket_13: false,
         }
     }
+}
 
-    fn advance(&mut self, next: u8) -> Result<bool, ()> {
-        use PotentialWebsocketState::*;
-        match &mut self.state {
-            ReadingHttpMethod { chars_read } => {
-                const METHOD: &'static str = "GET ";
-                if next == METHOD.bytes().nth(*chars_read as usize).unwrap() {
-                    *chars_read += 1;
-                    if *chars_read == METHOD.len() as u8 { self.state = ReadingHttpUri }
-                    Ok(false)
-                } else {
-                    Err(())
+fn assert_or_error(dat: bool) -> Result<(),()> { if dat { Ok(()) } else { Err(()) } }
+pub async fn open_websocket(mut socket: TcpStreamWrapper) -> Result<Websocket, ()> {
+    //Status line
+    assert_or_error(socket.next().await.ok_or(())? as char == 'G')?;
+    assert_or_error(socket.next().await.ok_or(())? as char == 'E')?;
+    assert_or_error(socket.next().await.ok_or(())? as char == 'T')?;
+    assert_or_error(socket.next().await.ok_or(())? as char == ' ')?;
+    //Rest of status line is irrelevant
+    while socket.next().await.ok_or(())? as char != '\n' {};
+
+    let mut wants_upgrade = false;
+    let mut wants_websocket = false;
+    let mut wants_websocket_13 = false;
+    let mut security_thing: Option<[u8; 16]> = None;
+    let mut header_name = String::new();
+    'header_loop: loop {
+        header_name.clear();
+        const HEADERS_FINISHED: &'static str = "\r\n";
+        const CONNECTION: &'static str = "connection: ";
+        const UPGRADE: &'static str = "upgrade: ";
+        const VERSION: &'static str = "sec-websocket-version: ";
+        const SECURITY: &'static str = "sec-websocket-key: ";
+        loop {
+            header_name.push((socket.next().await.ok_or(())? as char).to_lowercase().next().ok_or(())?);
+            if HEADERS_FINISHED.find(&header_name) == Some(0)
+            || CONNECTION.find(&header_name) == Some(0)
+            || UPGRADE.find(&header_name) == Some(0)
+            || VERSION.find(&header_name) == Some(0)
+            || SECURITY.find(&header_name) == Some(0) {
+                if header_name == HEADERS_FINISHED {
+                    break 'header_loop;
                 }
-            },
-
-            ReadingHttpUri => {
-                if next as char == ' ' { self.state = ReadingHttpVersion }
-                Ok(false)
-            },
-
-            ReadingHttpVersion => {
-                if next as char == '\n' { self.state = ReadingHttpHeaderName { name: String::default() } }
-                Ok(false)
-            },
-
-            ReadingHttpHeaderName { name } => {
-                if next as char == ':' {
-                    let header = name.trim();
-                    if header == "Upgrade" { self.state = ReadingHttpUpgradeHeader { value: String::default() } }
-                    else if header == "Sec-WebSocket-Key" { self.state = ReadingWsEncryptionHeader { value: String::default() } }
-                    else if header == "Connection" { self.state = ReadingHttpConnectionHeader { value: String::default() } }
-                    else if header == "Sec-Websocket-Version" { self.state = ReadingWsVersion { value: String::default() } }
-                    else { self.state = SkippingHttpHeader };
-                    Ok(false)
-                } else if name.len() > 20 {
-                    Err(())
-                } else {
-                    name.push(next as char);
-                    if name == "\r\n" {
-                        //Request over
-                        if self.wants_upgrade && self.wants_websocket && self.wants_websocket_13 && self.encryption_thing.is_some() {
-                            use sha::utils::{Digest, DigestExt};
-                            let encryption_response = base64::encode(&self.encryption_thing.unwrap()).to_string() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-                            let encryption_response = base64::encode(&sha::sha1::Sha1::default().digest(encryption_response.as_bytes()).to_bytes());
-                            let response = format!(
-                                "HTTP/1.1 101 Upgrade\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-Websocket-Accept: {}\r\n\r\n",
-                                encryption_response
-                            );
-                            self.state = PotentialWebsocketState::FlushingDataThenAccept { data: response.bytes().collect() };
-                        } else {
-                            return Err(())
+                if header_name == CONNECTION
+                || header_name == UPGRADE
+                || header_name == VERSION
+                || header_name == SECURITY {
+                    let mut value = String::with_capacity(40);
+                    let lowercase = header_name != SECURITY;
+                    loop {
+                        let mut dat = socket.next().await.ok_or(())? as char;
+                        if dat == '\n' { break }
+                        else if value.len() >= 40 { return Err(()) }
+                        else {
+                            if lowercase { dat = dat.to_lowercase().next().ok_or(())? };
+                            value.push(dat);
                         }
-                    };
-                    Ok(false)
-                }
-            },
-
-            SkippingHttpHeader => {
-                if next as char == '\n' { self.state = ReadingHttpHeaderName { name: String::default() } };
-                Ok(false)
-            },
-
-            ReadingWsEncryptionHeader { value } |
-            ReadingHttpUpgradeHeader { value } |
-            ReadingHttpConnectionHeader { value } |
-            ReadingWsVersion { value } => {
-                if next as char == '\n' {
-                    let header = value.trim().to_string();
-                    match &self.state {
-                        ReadingHttpUpgradeHeader { value } => {
-                            if header.contains("websocket") { self.wants_websocket = true; }
-                            else { return Err(()) }
-                        },
-                        ReadingWsEncryptionHeader { value } => {
-                            if let Ok(result) = base64::decode(header) {
-                                if result.len() == 16 {
-                                    let mut out = [0u8; 16];
-                                    for i in 0..16 { out[i] = result[i] };
-                                    self.encryption_thing = Some(out);
-                                } else { return Err(()) }
+                    }
+                    if header_name == CONNECTION {
+                        for token in value.trim().split(", ") { if token == "upgrade" { wants_upgrade = true; break; }; };
+                        if !wants_upgrade { return Err(()) };
+                    } else if header_name == UPGRADE {
+                        if value.trim() != "websocket" { return Err(()) };
+                        wants_websocket = true;
+                    } else if header_name == VERSION {
+                        if value.trim() != "13" { return Err(()) };
+                        wants_websocket_13 = true;
+                    } else if header_name == SECURITY {
+                        if let Ok(result) = base64::decode(value) {
+                            if result.len() == 16 {
+                                let mut out = [0u8; 16];
+                                for i in 0..16 { out[i] = result[i] };
+                                security_thing = Some(out);
                             } else { return Err(()) }
-                        },
-                        ReadingHttpConnectionHeader { value } => {
-                            if header.contains("Upgrade") { self.wants_upgrade = true; }
-                            else { return Err(()) }
-                        },
-                        ReadingWsVersion { value } => {
-                            if header == "13" { self.wants_websocket_13 = true; }
-                            else { return Err(()) }
-                        },
-                        _ => panic!()
-                    };
-                    self.state = ReadingHttpHeaderName { name: String::default() };
-                    Ok(false)
-                } else if value.len() > 40 {
-                    Err(())
-                } else {
-                    value.push(next as char);
-                    Ok(false)
+                        } else { return Err(()) }
+                    }
+                    break;
                 }
-            },
-
-            //FlushingData { data, next } => panic!("FlushingData state in advance method"),
-            FlushingDataThenAccept { data } => panic!("FlushingDataThenAccept state in advance method"),
-            Temp => panic!("Temp state in advance method"),
-        }
-    }
-
-    fn upgrade(&mut self) -> Websocket {
-        /*let mut buf = [0u8; 128];
-        for i in 0..initial_dat.len() {
-            buf[i] = initial_dat[i];
-        };*/
-        Websocket::new(std::mem::replace(&mut self.socket, None).unwrap())
-    }
-}
-
-impl Future for PotentialWebsocket {
-    type Output = Result<Websocket, ()>;
-    fn poll(mut self: Pin<&mut Self>, ctx: &mut Context) -> Poll<Self::Output> {
-        //if matches!(&self.state, PotentialWebsocketState::FlushingData { data, next }) ||
-        if matches!(&self.state, PotentialWebsocketState::FlushingDataThenAccept { data } ) {
-            let myself = self.deref_mut();
-            let data = match &mut myself.state {
-                //PotentialWebsocketState::FlushingData { data, next } => data,
-                PotentialWebsocketState::FlushingDataThenAccept { data } => data,
-                _ => panic!()
-            };
-            match Pin::new(myself.socket.as_mut().expect("Empty PotentialWebsocket")).poll_write(ctx, data.as_ref()) {
-                Poll::Ready(Ok(bytes_written)) => {
-                    drop(data.splice(0..bytes_written, Vec::with_capacity(0)));
-                    if data.len() < 1 {
-                        ctx.waker().wake_by_ref();
-                        /*if let PotentialWebsocketState::FlushingData { data, next } = &myself.state {
-                            if let PotentialWebsocketState::FlushingData { data, next } = std::mem::replace(&mut myself.state, PotentialWebsocketState::Temp) {
-                                myself.state = *next;
-                            } else {
-                                panic!();
-                            }
-                        } else {*/
-
-                        //Ready
-                        return Poll::Ready(Ok(self.upgrade()));
-                    };
-                    Poll::Pending
-                },
-                Poll::Ready(Err(_)) => Poll::Ready(Err(())),
-                Poll::Pending => Poll::Pending
-            }
-        } else {
-            let mut buf = [0u8; 512];
-            match Pin::new(self.socket.as_mut().expect("Empty PotentialWebsocket")).poll_read(ctx, &mut buf) {
-                Poll::Ready(Ok(bytes_read)) => {
-                    for i in 0..bytes_read {
-                        match self.advance(buf[i]) {
-                            //Ok(true) => return Poll::Ready(Ok(self.upgrade(&buf[i+1..bytes_read]))),
-                            Ok(_) => {
-                                //if matches!(&self.state, PotentialWebsocketState::FlushingData { data, next }) ||
-                                if matches!(&self.state, PotentialWebsocketState::FlushingDataThenAccept { data } ) {
-                                    ctx.waker().wake_by_ref();
-                                }
-                            },
-                            Err(_) => return Poll::Ready(Err(()))
-                        };
-                    };
-                    Poll::Pending
-                },
-                Poll::Ready(Err(_)) => Poll::Ready(Err(())),
-                Poll::Pending => Poll::Pending
             }
         }
     }
+    if !wants_upgrade || !wants_websocket || !wants_websocket_13 || security_thing.is_none() { return Err(()) };
+
+    use sha::utils::{Digest, DigestExt};
+    let encryption_response = base64::encode(security_thing.unwrap()).to_string() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    let encryption_response = base64::encode(&sha::sha1::Sha1::default().digest(encryption_response.as_bytes()).to_bytes());
+    let response = format!(
+        "HTTP/1.1 101 Upgrade\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-Websocket-Accept: {}\r\n\r\n",
+        encryption_response
+    ).as_bytes().to_vec();
+    socket.queue_send(Arc::new(response));
+    socket.flush().await;
+    todo!("We made it!");
 }
 
 pub struct Websocket {
-    socket: TcpStream,
-    buf_out: Vec<Arc<Vec<u8>>>,
-    incoming: WebsocketIncoming,
+    socket: TcpStreamWrapper,
 }
 
 pub enum WebsocketOpCode {
