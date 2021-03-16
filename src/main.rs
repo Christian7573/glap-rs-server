9d212c59f337f6dcb5d1323611a4b7f761cee5e3
Christian7573 <cag4561@gmail.com>
2021-03-16 18:24:08 -0500

    Refractored teleport command

--- src/main.rs
+++ src/main.rs
@@ -497,26 +497,18 @@ async fn main() {
                 let chunks: Vec<String> = command.split_whitespace().map(|s| s.to_string()).collect();
                 match chunks[0].as_str() {
                     "/teleport" => {
-                        if(chunks.len() == 3) {
-                            if(is_string_numeric(chunks[1].to_string()) && is_string_numeric(chunks[2].to_string())) {
-                                let x: f32 = chunks[1].parse().unwrap();
-                                let y: f32 = chunks[2].parse().unwrap();
-                                if let Some((player_meta, core)) = players.get_mut(&id) {
-                                    let core_pos = simulation.world.get_rigid(MyHandle::Part(core.body_id)).unwrap().position().translation;
-                                    fn teleport(part: &world::parts::Part, simulation: &mut world::Simulation, x: f32, y: f32, core_x: f32, core_y: f32) {
-                                        simulation.world.get_rigid_mut(MyHandle::Part(part.body_id)).unwrap().set_position(Isometry2::new(Vector2::new(x, y), 0.0));
-                                        for part in part.attachments.iter() {
-                                            if let Some((part, _, _)) = part {
-                                                let part_pos = simulation.world.get_rigid(MyHandle::Part(part.body_id)).unwrap().position().translation;
-                                                let offset_x = part_pos.x - core_x;
-                                                let offfset_y = part_pos.y - core_y;
-                                                teleport(part, simulation, x + offset_x, y + offfset_y, core_x, core_y);
-                                            }
-                                        }
-                                    }
-                                    simulation.world.get_rigid_mut(MyHandle::Part(core.body_id)).unwrap().set_position(Isometry2::new(Vector2::new(x, y), 0.0));
-                                    teleport(core, &mut simulation, x, y, core_pos.x, core_pos.y);
+                        if chunks.len() == 3 {
+                            if let (Ok(x), Ok(y)) = (chunks[1].parse::<f32>(), chunks[2].parse::<f32>()) {
+                                let teleport_to = Vector2::new(x, y);
+                                if let Some(player_meta) = players.get_mut(&id) {
+                                    let core_pos = simulation.world.get_rigid(player_meta.core).unwrap().position().translation.vector;
                                     println!("Teleporting {} to: {} {}", player_meta.name, x, y);
+                                    simulation.world.recurse_part_mut(player_meta.core, 0, 0, AttachedPartFacing::Up, AttachedPartFacing::Up, |_, part: &world::parts::Part, _, _, _, _| {
+                                        part.body_mut().set_position(Isometry2::new(
+                                                part.body().position().clone().translation.vector - core_pos + teleport_to,
+                                                part.body().position().rotation.angle()
+                                        ));
+                                    });
                                 }
                             }
                         }
